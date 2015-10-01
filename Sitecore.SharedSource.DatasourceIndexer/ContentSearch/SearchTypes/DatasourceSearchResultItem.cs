e3b42c42d5077855d30b378b07e601b02bf3f44c
Rob Maas | Estate <r.maas@estate.nl>
2015-10-02 00:08:53 +0200

    Nuget fixed

---
+++ Sitecore.SharedSource.DatasourceIndexer/ContentSearch/SearchTypes/DatasourceSearchResultItem.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.ContentSearch;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.SharedSource.DatasourceIndexer.ContentSearch.SearchTypes
+{
+    public class DatasourceSearchResultItem : Sitecore.ContentSearch.SearchTypes.SearchResultItem
+    {
+        [IndexField("datasource_content")]
+        public string DatasourceContent { get; set; }
+    }
+}
